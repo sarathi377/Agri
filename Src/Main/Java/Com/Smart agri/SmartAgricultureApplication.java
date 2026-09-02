2be8a934f2bd4258301051f74118aad826939adb
Keerthasarathi G K <keerthasarathi@gmail.com>
2026-09-02 19:16:30 +0530

    Add SmartAgricultureApplication class for Spring Boot

---
+++ Src/Main/Java/Com/Smart agri/SmartAgricultureApplication.java
@@ -0,0 +1,9 @@
+package com.smartagri; 
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+@SpringBootApplication 
+public class SmartAgricultureApplication { 
+public static void main(String[] args){
+SpringApplication.run(SmartAgricultureApplication.class,args);
+} 
+}
